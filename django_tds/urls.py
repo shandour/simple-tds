d86508836c5a1faa1b49b0421e9280c5c58c2602
shandour <shandour>
2019-08-17 22:18:40 +0300

    / redirects to management/

--- django_tds/urls.py
+++ django_tds/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include, re_path
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 from tds.urls import (
     api_patterns as tds_api_patterns,
@@ -11,7 +11,9 @@ api_patterns = user_api_patterns + tds_api_patterns
 
 urlpatterns = [
     path('api/', include(api_patterns)),
+    path('', RedirectView.as_view(pattern_name='index_view')),
     re_path('management.*',
-            TemplateView.as_view(template_name='index.html')),
+            TemplateView.as_view(template_name='index.html'),
+            name='index_view'),
     re_path(r'^(?P<url>[0-9a-zA-z]+)$', landing_page),
 ]
